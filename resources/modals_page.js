a8a1200821f8972ccf57fa6fc4ed12a5c8e69f8b
Jason Dooley <jd@sevif.me>
2011-08-22 22:25:29 -0400

    Add a couple pages

---
+++ resources/modals_page.js
@@ -0,0 +1,16 @@
+// ==========================================================================
+// Project:   Brochurno.modalsPage
+// Copyright: @2011 My Company, Inc.
+// ==========================================================================
+/*globals Brochurno */
+
+// This page describes a part of the interface for your application.
+Brochurno.modalsPage = SC.Page.design({
+
+  // Add your views here.  Ex:
+  
+  // mainView: SC.View.design({
+  //   layout: { top: 0, left: 0, right: 0, height: 0 }
+  // })
+
+});
